da6378d1ba2a255f1c200296a1f53101807d92a2
RL-55 <rickyl55@nycstudents.net>
2025-10-28 09:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,46 @@
 import json
-## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
 def yearafter():
-    z=-1
+    z=0
     y=int(input("Select year "))
     for h in data:
-        z+=1
         x=data[z]["year"]
-        if x>=y:
+        if x>y:
             print(data[z]["title"],data[z]["year"])
+        z+=1
 def yearbefore():
-    z=-1
+    z=0
     y=int(input("Select year "))
+    w=int(input("Select year "))
     for h in data:
-        z+=1
         x=data[z]["year"]
-        if x>=y:
+        if x>y>w:
             print(data[z]["title"],data[z]["year"])
-    z=-1
-    y=int(input("Select year "))
-    for h in data:
         z+=1
-        x=data[z]["year"]
-        if x<=y:
-            print(data[z]["title"],data[z]["year"])
 def yearduring():
-    z=-1
+    z=0
     y=int(input("Select year "))
     for h in data:
-        z+=1
         x=data[z]["year"]
         if x==y:
             print(data[z]["title"],data[z]["year"])
+        z+=1
 def moviesearch():
     y=input("Search a movie ")
-    z=-1
+    z=0
     for h in data:
-        z+=1
         x=data[z]["title"]
         if x==y:
             print(data[z])
+    z+=1
 def genresearch():
+    z=0
     y=input("Search a genre ")
-    z=-1
     for h in data:
+        x=data[z]["genres"]
+        for i in x:
+            if i==y:
+                print(data[z]["title"],data[z]["genres"])
         z+=1
-        x=data[z]["genres"][0]
-        if x==y:
-            print(data[z]["title"],data[z]["genres"])
-genresearch()
