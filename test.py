9ef5109142b4f752cbf278639ba32d6093e72fcd
RL-55 <rickyl55@nycstudents.net>
2025-10-30 09:26:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-import tkinter
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=500)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=10, row=20)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=100, row=200)
-root.mainloop()
+import tkinter as tk
+root=tk.Tk()
