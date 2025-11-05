e55d5b1693258c7802966ed6d0cc22778196f715
RL-55 <rickyl55@nycstudents.net>
2025-11-05 09:26:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import tkinter as tk
 root=tk.Tk()
+root.geometry("500x450+700+280") 
+root.title("Let's go gambling!")
+button=tk.Button(root,text="Roll",width=10)
+button.place(x=210,y=200)
+button.grid_location(x=10,y=10)
+root.mainloop()
