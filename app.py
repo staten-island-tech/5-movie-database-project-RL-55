211b28d72d8a5b63b4265864c63ea27da5d34b33
RL-55 <rickyl55@nycstudents.net>
2025-11-10 09:18:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,44 +4,63 @@ data = json.load(movies)
 
 def yearafter():
     z=0
+    No=False
     y=int(input("Select year "))
     for h in data:
         x=data[z]["year"]
         if x>y:
             print(data[z]["title"],data[z]["year"])
+            No=True
         z+=1
-def yearbefore():
+    if No==False:
+        print("No Results")
+def yearbetween():
     z=0
-    y=int(input("Select year "))
-    w=int(input("Select year "))
+    No=False
+    y=int(input("Select year start "))
+    w=int(input("Select year end "))
     for h in data:
         x=data[z]["year"]
-        if x>y>w:
+        if w>x>y:
             print(data[z]["title"],data[z]["year"])
+            No=True
         z+=1
+    if No==False:
+        print("No Results")
 def yearduring():
     z=0
     y=int(input("Select year "))
+    No=False
     for h in data:
         x=data[z]["year"]
         if x==y:
             print(data[z]["title"],data[z]["year"])
+            No=True
         z+=1
+    if No==False:
+        print("No Results")
 def moviesearch():
     y=input("Search a movie ")
     z=0
+    No=False
     for h in data:
         x=data[z]["title"]
-        if x==y:
+        if x.lower()==y.lower():
             print(data[z])
-    z+=1
+            No=True
+        z+=1
+    if No==False:
+        print('No Result')
 def genresearch():
     z=0
+    No=False
     y=input("Search a genre ")
     for h in data:
         x=data[z]["genres"]
         for i in x:
-            if i==y:
+            if i.lower()==y.lower():
                 print(data[z]["title"],data[z]["genres"])
         z+=1
-
+    if No==False:
+        print("No Result")
+genresearch()
